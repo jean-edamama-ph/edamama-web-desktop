9e73cbebcce910b1a0680459613b888365299251
jean <jean@edamama.ph>
2024-01-18 20:03:09 +0800

    removing 'thisTest'

--- libraries/tests/regressionTestSuite_web/profile/test_myGifts.py
+++ libraries/tests/regressionTestSuite_web/profile/test_myGifts.py
@@ -14,7 +14,6 @@ import libraries.util.profile.myProfile as uMyProfile
 """ Author: rmakiling_20231008 Execution Time: 54s - 59s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that the user is able to add producs to the Gift List')
 def test_ACQ_AUTO_1053_User_should_be_able_to_add_products_to_the_gift_list(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -40,7 +39,6 @@ def test_ACQ_AUTO_1053_User_should_be_able_to_add_products_to_the_gift_list(page
     
 """ Author: rmakiling_20231012 Execution Time: 40s - 47s """
 @pytest.mark.regressionTestSuite()
-@pytest.mark.acquiTestSuite()
 @pytest.mark.thisTest()
 @allure.step('To verify that user is able to archive and activate the gift list.')
 def test_ACQ_AUTO_1056_And_AUTO_1049_User_should_be_able_to_archive_activate_the_gift_list(page):
@@ -76,7 +74,6 @@ def test_ACQ_AUTO_1056_And_AUTO_1049_User_should_be_able_to_archive_activate_the
 """ Author: jatregenio_20231219 Execution Time: 35s - 40s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that user is able to create a gift list.')
 def test_ACQ_AUTO_1059_User_should_be_able_to_create_a_gift_list(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -103,7 +100,6 @@ def test_ACQ_AUTO_1059_User_should_be_able_to_create_a_gift_list(page):
 """ Author: jatregenio_20231219 Execution Time: 40s - 42s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that the gift list can be edited.')
 def test_ACQ_AUTO_1062_User_should_be_able_to_edit_a_gift_list(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -127,7 +123,6 @@ def test_ACQ_AUTO_1062_User_should_be_able_to_edit_a_gift_list(page):
 """ Author: jatregenio_20231225 Execution Time: 1m 13s - 1m 19s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that user is able to remove item from gift list.')
 def test_ACQ_AUTO_1065_User_should_be_able_to_remove_an_item_from_gift_list(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -149,7 +144,6 @@ def test_ACQ_AUTO_1065_User_should_be_able_to_remove_an_item_from_gift_list(page
 """ Author: jatregenio_20240111 Execution Time: 33s - 35s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that user is able to delete the gift list.')
 def test_ACQ_AUTO_1068_User_should_be_able_to_remove_the_gift_list(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -173,7 +167,6 @@ def test_ACQ_AUTO_1068_User_should_be_able_to_remove_the_gift_list(page):
 """ Author: jatregenio_20240105 Execution Time: 26s - 28s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that the user should not be able to create a gift list if mandatory fields are blank.')
 def test_ACQ_AUTO_1071_User_should_not_be_able_to_create_a_gift_list_when_mandatory_fields_are_blank(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
@@ -191,7 +184,6 @@ def test_ACQ_AUTO_1071_User_should_not_be_able_to_create_a_gift_list_when_mandat
 """ Author: jatregenio_20240113 Execution Time: 00s - 00s """
 @pytest.mark.regressionTestSuite()
 @pytest.mark.acquiTestSuite()
-@pytest.mark.thisTest()
 @allure.step('To verify that the user should not be able to create a gift list without selecting an address.')
 def test_ACQ_AUTO_1074_User_should_not_be_able_to_create_a_gift_list_when_mandatory_fields_are_blank(page):
     uCommon.log(0, 'Step 1 - Open edamama website')
